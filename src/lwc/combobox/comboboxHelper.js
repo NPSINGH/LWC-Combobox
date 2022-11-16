087c2fc823c0c5a10a0700d4e018cd6f0b2e19d7
Np S <npsingh.1991@gmail.com>
2022-11-16 14:34:36 +0530

    Update Helper to fix reactivity issues

--- src/lwc/combobox/comboboxHelper.js
+++ src/lwc/combobox/comboboxHelper.js
@@ -3,22 +3,23 @@ export function deepClone(obj, hash = new WeakMap()) {
 	if (Object(obj) !== obj) return obj; // primitives
 	if (hash.has(obj)) return hash.get(obj); // cyclic reference
 	const result = obj instanceof Set ? new Set(obj) // See note about this!
-				 : obj instanceof Map ? new Map(Array.from(obj, ([key, val]) => 
-										[key, deepClone(val, hash)])) 
-				 : obj instanceof Date ? new Date(obj)
-				 : obj instanceof RegExp ? new RegExp(obj.source, obj.flags)
-				 // ... add here any specific treatment for other classes ...
-				 // and finally a catch-all:
-				 : obj.constructor ? new obj.constructor() 
-				 : Object.create(null);
+		     : obj instanceof Map ? new Map(Array.from(obj, ([key, val]) => 
+					    [key, deepClone(val, hash)])) 
+		     : obj instanceof Date ? new Date(obj)
+		     : obj instanceof RegExp ? new RegExp(obj.source, obj.flags)
+		     // ... add here any specific treatment for other classes ...
+		     // and finally a catch-all:
+		     : obj instanceof File ? new File([obj],obj.name,{ type: obj.type })
+		     : obj.constructor ? new obj.constructor() 
+		     : Object.create(null);
 	hash.set(obj, result);
 	return Object.assign(result, ...Object.keys(obj).map(
-		key => ({ [key]: deepClone(obj[key], hash) }) ));
+	    key => ({ [key]: deepClone(obj[key], hash) }) ));
 }
     
 export const uniqueStrGenerator = () => {
-        return Date.now().toString(36) + Math.random().toString(36).substr(2);
-    };
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+};
 
 export const openAndCloseDropdown = (opr = 'close',compObj)=>{
     let template = compObj.template;
@@ -111,6 +112,8 @@ export function syncComboboxAttributes(compObj){
         inputElement.readOnly = compObj.comboboxObj.readOnly;
     }
     else{
+        inputElement.disabled = compObj.comboboxObj._disabled;
+        inputElement.readOnly = compObj.comboboxObj.readOnly;
         if(compObj.comboboxObj.autocomplete){
             inputElement.readOnly = false;
             if(compObj.comboboxObj._clearMode){
